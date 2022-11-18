9d0e6ff7c6fd8d00c61a5f07f5295eab38bc4133
Ding Li <dli@nexant.com>
2022-11-18 09:08:06 -0800

    add token based security

--- main.py
+++ main.py
@@ -2,17 +2,94 @@ import json
 import pathlib
 from typing import List, Union
 
-from fastapi import FastAPI, Response, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI, Response, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
 
 from models import Track
 
 # https://www.bugbytes.io/posts/creating-a-music-track-api-with-fastapi-in-python/
 
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
 # instantiate the FastAPI app
 app = FastAPI()
 
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
+
 
 # create container for our data - to be loaded at app startup.
 data = []
@@ -29,7 +106,7 @@ async def startup_event():
 
 @app.get('/tracks/', response_model=List[Track])
 def tracks(token: str = Depends(oauth2_scheme)):
-    return {"token": token, "data": data}
+    return data
 
 
 @app.get('/tracks/{track_id}/', response_model=Union[Track, str])
